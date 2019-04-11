4efde17d99df05e84a7fd94e2340abb0efa335dc
GeonU <double_k2@naver.com>
2019-04-11 17:04:08 +0900

    MyPage ListView Item setClick Event
    * add MyPage ListView Item setClick Event

--- app/src/main/java/io/kong/mypetdiary/adapter/MyPageListViewAdapter.java
+++ app/src/main/java/io/kong/mypetdiary/adapter/MyPageListViewAdapter.java
@@ -1,6 +1,7 @@
 package io.kong.mypetdiary.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,7 +17,15 @@ import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 import io.kong.mypetdiary.R;
+import io.kong.mypetdiary.activity.AddPetActivity;
+import io.kong.mypetdiary.activity.SelectMyPetActivity;
 import io.kong.mypetdiary.item.MyPageListViewItem;
+
+import static io.kong.mypetdiary.item.MyPageListViewItem.EXTRA_PET_BIRTH;
+import static io.kong.mypetdiary.item.MyPageListViewItem.EXTRA_PET_COME;
+import static io.kong.mypetdiary.item.MyPageListViewItem.EXTRA_PET_KIND;
+import static io.kong.mypetdiary.item.MyPageListViewItem.EXTRA_PET_NAME;
+import static io.kong.mypetdiary.item.MyPageListViewItem.EXTRA_PET_URL;
 
 public class MyPageListViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
@@ -26,6 +35,9 @@ public class MyPageListViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     private ArrayList<MyPageListViewItem> listViewItems = new ArrayList<>();
 
     Context context;
+
+    String getImageUrl, stPetName, stPetBirth, stPetUrl, stPetCome;
+    int petKind;
 
     @NonNull
     @Override
@@ -72,7 +84,7 @@ public class MyPageListViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
-        MyPageListViewItem item = listViewItems.get(position);
+        final MyPageListViewItem item = listViewItems.get(position);
 
         if (holder instanceof ViewHolder) {
             int year = Integer.parseInt(item.getStPetBirth().substring(0, 4));
@@ -85,8 +97,34 @@ public class MyPageListViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
                     .skipMemoryCache(true).into(((ViewHolder) holder).imvPet);
             ((ViewHolder) holder).txtPetName.setText(item.getStPetName());
             ((ViewHolder) holder).txtBirth.setText(Integer.toString(d_day) + "일 째");
-        } else {
 
+            ((ViewHolder) holder).itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    stPetUrl = item.getImgPetUri();
+                    stPetName = item.getStPetName();
+                    stPetBirth = item.getStPetBirth();
+                    stPetCome = item.getStPetCome();
+                    petKind = item.getStPetKind();
+
+                    Intent intent = new Intent(context, SelectMyPetActivity.class);
+                    intent.putExtra(EXTRA_PET_URL, stPetUrl);
+                    intent.putExtra(EXTRA_PET_NAME, stPetName);
+                    intent.putExtra(EXTRA_PET_BIRTH, stPetBirth);
+                    intent.putExtra(EXTRA_PET_COME, stPetCome);
+                    intent.putExtra(EXTRA_PET_KIND, petKind);
+                    context.startActivity(intent);
+
+                }
+            });
+        } else {
+            ((ViewHolderLast) holder).itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, AddPetActivity.class);
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 
